01fe3b974dadc3f917de83f4d1a7624723868bda
Mikhail Chugaev <mixachugaev@gmail.com>
2024-03-01 12:08:47 +0300

    Добавлено задание 5 лекции

---
+++ lecture05/index.js
@@ -0,0 +1,7 @@
+import { Chugaev } from 'chugaev-lecture04'
+const chugaev = new Chugaev('Сделал задание 4 лекции.');
+chugaev.checkIn();
+
+import { SecondName } from 'spesivtsev'
+const person = new SecondName('Спесивцев', 'Привет!');
+person.checkIn();
