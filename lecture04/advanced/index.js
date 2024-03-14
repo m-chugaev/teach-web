5f973431347aac807b628b7a3b6d88fec94808fc
AfordiumSX <104629059+AfordiumSX@users.noreply.github.com>
2024-03-14 19:10:31 +0500

    Update index.js

--- lecture04/advanced/index.js
+++ lecture04/advanced/index.js
@@ -3,7 +3,7 @@ import { Chugaev } from 'chugaev-lecture04'
 const chugaev = new Chugaev('Сделал задание 4 лекции.');
 chugaev.checkIn();
 
-import { SecondName } from 'rychin'
+import { SecondName } from 'lecture4'
 
 const person = new SecondName('Рычин', 'Привет!');
 person.checkIn();
