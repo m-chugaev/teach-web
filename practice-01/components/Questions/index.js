7af685d79cefa2d791b5723f595d00a74224ecac
skamoteon <stravvhat@mail.ru>
2024-03-15 14:51:37 +0500

    выполнено задание 20

--- practice-01/components/Questions/index.js
+++ practice-01/components/Questions/index.js
@@ -7,7 +7,17 @@ export default function Questions() {
             <h2 className={styles.title}>Вопросы для собеседования</h2>
             <ol className={styles.list}>
                 {questions.map(function(question, index) {
-                    return <li key={index}>{question.text}</li>;
+                    return <div className={styles.ac} key={index}>
+                        <div className={styles.faqList}>
+                            <div className={styles.accord}>
+                                <div data-w-id="4c10fe5b-3460-50ad-990c-41ff238f36c8" className={styles.question}>
+                                    <div className={styles.large}>
+                                        {question.text}
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>;
                 })}
             </ol>
         </div>
