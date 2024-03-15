44ff71dc4b851586d343bd0f1bc1774711996b0a
Mikhail Chugaev <mixachugaev@gmail.com>
2024-03-15 14:00:14 +0300

    fix

--- practice-01/app/page.js
+++ practice-01/app/page.js
@@ -3,6 +3,7 @@ import styles from "./page.module.scss";
 import Questions from '../components/Questions';
 import JokeNotice from '../components/JokeNotice';
 import RandomQuestions from '../components/RandomQuestions';
+import RandomQuestionsFrequency from '../components/RandomQuestionsFrequency';
 import Comment from '../components/Comment/Comment';
 import SwitchTheme from "../components/SwitchTheme";
 
@@ -11,6 +12,7 @@ export default function Home() {
     <main className={styles.main}>
       <Questions />
       <RandomQuestions />
+      <RandomQuestionsFrequency />
       <JokeNotice />
       <SwitchTheme />
       <Image
