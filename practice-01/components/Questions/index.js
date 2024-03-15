dad77d144d2e8fedd28a89b2a55934a086dacee9
Mikhail Chugaev <mixachugaev@gmail.com>
2024-03-15 13:29:01 +0300

    fix styles

--- practice-01/components/Questions/index.js
+++ practice-01/components/Questions/index.js
@@ -5,21 +5,21 @@ export default function Questions() {
     return (
         <div className={styles.container}>
             <h2 className={styles.title}>Вопросы для собеседования</h2>
-            <ol className={styles.list}>
+            <div className={styles.list}>
                 {questions.map(function(question, index) {
                     return <div className={styles.ac} key={index}>
                         <div className={styles.faqList}>
                             <div className={styles.accord}>
                                 <div data-w-id="4c10fe5b-3460-50ad-990c-41ff238f36c8" className={styles.question}>
                                     <div className={styles.large}>
-                                        <ol>{question.text}</ol>
+                                        <span>{index+1}. {question.text}</span>
                                     </div>
                                 </div>
                             </div>
                         </div>
                     </div>;
                 })}
-            </ol>
+            </div>
         </div>
     );
 }
