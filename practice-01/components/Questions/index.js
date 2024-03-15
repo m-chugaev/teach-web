6f839a93cfdd548ad94072db68bb6a2c56c3a9e9
ААА <zikzakbym@gmail.com>
2024-03-15 14:58:38 +0500

    выполнено задание 19

--- practice-01/components/Questions/index.js
+++ practice-01/components/Questions/index.js
@@ -1,15 +1,38 @@
-import styles from './Questions.module.scss';
-import questions from "../../questions.js"
-
+import { useState } from 'react';
+import styles from './Questions.module.scss'; 
+import questions from "../../questions.js" 
+ 
 export default function Questions() {
-    return (
-        <div className={styles.container}>
-            <h2 className={styles.title}>Вопросы для собеседования</h2>
-            <ol className={styles.list}>
-                {questions.map(function(question, index) {
-                    return <li key={index}>{question.text}</li>;
-                })}
-            </ol>
-        </div>
-    );
+    const [filter, setFilter] = useState("");
+    
+
+    const handleFilterChange = (e) => {
+        setFilter(e.target.value);
+    };
+
+    const filteredQuestions = questions.filter(question => {
+        return question.category.toLowerCase().includes(filter.toLowerCase());
+    });
+
+    return ( 
+        <div className={styles.container}> 
+            <h2 className={styles.title}>Вопросы для собеседования</h2> 
+            
+            <div className={styles.filterContainer}>
+                <label htmlFor="filter">Фильтр по категории:</label>
+                <input 
+                    type="text" 
+                    id="filter" 
+                    value={filter}
+                    onChange={handleFilterChange} 
+                />
+            </div>
+            
+            <ol className={styles.list}> 
+                {filteredQuestions.map(function(question, index) { 
+                    return <li key={index}>{question.text}</li>; 
+                })} 
+            </ol> 
+        </div> 
+    ); 
 }
