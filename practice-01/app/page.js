5fdcc5ec4c9b78704c52471aeecea5959d8c3620
Mikhail Chugaev <mixachugaev@gmail.com>
2024-03-15 10:35:42 +0300

    Выполнение задания 3

--- practice-01/app/page.js
+++ practice-01/app/page.js
@@ -1,11 +1,13 @@
 import Image from "next/image";
 import styles from "./page.module.scss";
 import Questions from '../components/Questions';
+import RandomQuestions from '../components/RandomQuestions';
 
 export default function Home() {
   return (
     <main className={styles.main}>
       <Questions />
+      <RandomQuestions />
       <Image
         className={styles.icon}
         src="/icons/next.svg"
