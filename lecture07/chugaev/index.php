a365334a4e536ed66bab1adcb780de340aac50a1
Mikhail Chugaev <mixachugaev@gmail.com>
2024-03-22 14:21:16 +0300

    Пример для лекции 7

---
+++ lecture07/chugaev/index.php
@@ -0,0 +1,46 @@
+<?php
+
+// Логика формы и её валидации
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Form</title>
+
+    <!-- Стили уже подключены -->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/mini.css/3.0.1/mini-default.css">
+</head>
+
+<body>
+    <!-- Эту форму нужно обновить -->
+    <form action="" class="">
+        <input type="text">
+        <input type="text">
+        <select name="" id="">
+            <option value=""></option>
+            <option value=""></option>
+            <option value=""></option>
+        </select>
+        <input type="checkbox" name="" id="">
+        <button type="submit"></button>
+    </form>
+
+    <style>
+        body {
+            font-family: PT Sans, sans-serif;
+            color: #464646;
+            background-color: #eee;
+            padding: 16px;
+            margin: 0 auto;
+            width: 100%;
+            max-width: 400px;
+        }
+    </style>
+</body>
+
+</html>
