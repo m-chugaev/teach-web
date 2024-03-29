11892b8ec97882f226d762f39f08067ec6a91ce0
Nadia <cherenadia2003@gmail.com>
2024-03-29 15:04:03 +0500

    fix merge

--- practice-01/app/page.js
+++ practice-01/app/page.js
@@ -1,11 +1,8 @@
 import Image from "next/image";
 import styles from "./page.module.scss";
 import Questions from '../components/Questions';
-<<<<<<< HEAD
 import QuestionsList from '../components/QuestionsList';
-=======
 import JokeNotice from '../components/JokeNotice';
->>>>>>> main
 import RandomQuestions from '../components/RandomQuestions';
 import RandomQuestionsFrequency from '../components/RandomQuestionsFrequency';
 import Comment from '../components/Comment/Comment';
@@ -16,14 +13,11 @@ export default function Home() {
     <main className={styles.main}>
       <Questions />
       <RandomQuestions />
-<<<<<<< HEAD
       <QuestionsList />
-=======
       <RandomQuestionsFrequency />
       <JokeNotice />
       <SwitchTheme />
       <Random5Questions />
->>>>>>> main
       <Image
         className={styles.icon}
         src="/icons/next.svg"
