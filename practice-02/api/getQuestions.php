c7ff829108e5aa71e856b2f1c7f86a3f4802320b
Mikhail Chugaev <mixachugaev@gmail.com>
2024-03-29 13:24:10 +0300

    Код с лекции 10

---
+++ practice-02/api/getQuestions.php
@@ -0,0 +1,5 @@
+<?php
+include '../questions.php';
+
+header('Content-Type: application/json; charset=utf-8');
+echo json_encode($questions);
---
+++ practice-02/index.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/mini.css/3.0.1/mini-default.css">
+    <title>WEB Development</title>
+</head>
+<body class="responsive-padding">
+    <h1>Вопросы для собеседования</h1>
+    <?php include './components/questions/index.php'; ?>
+</body>
+</html>
