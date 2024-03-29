d71bac7dda3191e7183d31afc42f637c570a334c
Nadia <cherenadia2003@gmail.com>
2024-03-29 15:06:41 +0500

    new component

--- practice-01/app/page.js
+++ practice-01/app/page.js
@@ -1,7 +1,7 @@
 import Image from "next/image";
 import styles from "./page.module.scss";
 import Questions from '../components/Questions';
-import QuestionsList from '../components/QuestionsList';
+import RandomQuestionsList from '../components/RandomQuestionsList';
 import JokeNotice from '../components/JokeNotice';
 import RandomQuestions from '../components/RandomQuestions';
 import RandomQuestionsFrequency from '../components/RandomQuestionsFrequency';
@@ -13,7 +13,7 @@ export default function Home() {
     <main className={styles.main}>
       <Questions />
       <RandomQuestions />
-      <QuestionsList />
+      <RandomQuestionsList />
       <RandomQuestionsFrequency />
       <JokeNotice />
       <SwitchTheme />
