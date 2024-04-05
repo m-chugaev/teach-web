18c999fd4d4517d2ee2fb248d3eab41840776cac
Mikhail Chugaev <mixachugaev@gmail.com>
2024-04-05 10:48:08 +0300

    fix

--- practice-02/components/questions/index.php
+++ practice-02/components/questions/index.php
@@ -1,5 +1,5 @@
 <div class="responsive-padding">
-    <h1>Вопросы для собеседования!!</h1>
+    <h1>Вопросы для собеседования</h1>
 
     <ol class="js-questions"></ol>
     <div class="js-pagination" style="margin-top: 20px; text-align: center;">
