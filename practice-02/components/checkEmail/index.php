b96e82aefec9253e2251cd76d2d1b43aedc68cae
Mikhail Chugaev <mixachugaev@gmail.com>
2024-04-12 14:56:19 +0300

    f

--- practice-02/components/checkEmail/index.php
+++ practice-02/components/checkEmail/index.php
@@ -10,12 +10,10 @@ $latest = $service->getLatest();
 ?>
 
 <div class="check_email">
-    
     <h2>Чекни свой email</h2>
-
     <div>
         <form class="formEmail_js" method="POST">
-            Email: <input type="text" name="email" value="<?=$latest?>"> <!-- Тут в value нужно передать сохранение -->
+            Email: <input type="text" name="email" value="<?= $latest ?>"> <!-- Тут в value нужно передать сохранение -->
             <input type="submit" value="Submit">
         </form>
     </div>
